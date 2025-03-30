2f0f569bfe2d91b27ce9ddd5c67985d363da0aca
lucashomero <lucashomerovs@gmail.com>
2025-03-29 21:10:44 -0300

    syntax, var, hoisting

--- syntax/script.js
+++ syntax/script.js
@@ -1,2 +1,35 @@
+//  JavaScript - Syntax
 console.log("Hello World") 
-console.log("Conectado com Html")
+console.log("Js conectado com Html")
+
+// Var
+// variavel sem valor
+var user
+// atribuindo valor a variavel
+var user = "lucashomero@gmail.com"
+console.log(email)
+// substituindo o valor de uma variavel
+email = "kirlianshores@gmail.com"
+console.log(email)
+
+//Let
+
+// joao
+// let joao
+// let joao = "Lucas"
+// Let nao permite redlecarar variavel no mesmo escop
+// Let icada (hoisting) porem n pode ser usada antes da declaracao
+
+// Const
+// N permite reatribuicao
+// const num = 3
+// const num = 2
+
+// Escopo global
+// var email = "joao62@gmail.com"
+
+// Escopo local
+// {
+//     console.log (email)
+// }
+
