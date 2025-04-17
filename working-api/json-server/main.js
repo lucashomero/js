a6c8012242503b67066d86aa95b62b8f9cbcaa4d
lucashomero <lucashomerovs@gmail.com>
2025-04-17 11:34:02 -0300

    feat: async function fetchProductsById()

--- working-api/json-server/main.js
+++ working-api/json-server/main.js
@@ -1,11 +1,19 @@
 // Utilizando fetch com then
+// Utilizamos quando n queremos criar uma funcao nova
 // const response = fetch("http://localhost:3333/products").then((response) => response.json()).then((data) => console.log(data))
 
 // Utilizando fetch com asynca/wait
+// Utilizar qnd queremos criar funcao
 async function fetchProducts() {
     const response = await fetch("http://localhost:3333/products")
     const data = await response.json()
     console.log(data)
 }
-
 fetchProducts()
+
+async function fetchProductsById(id) {
+    const response = await fetch(`http://localhost:3333/products/${id}`)
+    const data = await response.json()
+    console.log(data)
+}
+fetchProductsById("2")
