939eea7c5ac19b9afd11bb3a461e51489c42defb
lucashomero <lucashomerovs@gmail.com>
2025-04-17 15:56:43 -0300

    scripts: add Babel watch mode to auto-compile on changes

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ class User {
     constructor({ email }) {
         this.email = email
     }
+
+    sendMessage(){
+        console.log("mensagem enviada para:", this.email)
+    }
  }
 
  let user = new User ({email: "lucas@email.com"})
+ user.sendMessage()
