3a6549fb348943f59502fa49f5b91efb8e419def
lucashomero <lucashomerovs@gmail.com>
2025-04-17 17:12:17 -0300

    build: setup Webpack with HTML, CSS, and Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: path.resolve(__dirname, "bundlers", "src", "js", "index.js"),
@@ -6,5 +7,25 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
-    mode: "development"
+    mode: "development",
+    plugins: [new HTMLWebpackPlugin()],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+            exclude: "/node_modules", 
+        },
+        {
+            test: /\.js$/i,
+            exclude: "/node_modules",
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: [["@babel/preset-env", {targets: "defaults"}]],
+                }
+            }
+        }
+    ],
+    },
+    
 }
