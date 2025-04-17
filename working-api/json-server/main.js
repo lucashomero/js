f07bc6b5c71b8300842fa27e7c9a5e2bfc97707b
lucashomero <lucashomerovs@gmail.com>
2025-04-17 11:57:01 -0300

    feat: fetch w/ POST

--- working-api/json-server/main.js
+++ working-api/json-server/main.js
@@ -9,11 +9,32 @@ async function fetchProducts() {
     const data = await response.json()
     console.log(data)
 }
-fetchProducts()
+// fetchProducts()
 
 async function fetchProductsById(id) {
     const response = await fetch(`http://localhost:3333/products/${id}`)
     const data = await response.json()
     console.log(data)
 }
-fetchProductsById("2")
+// fetchProductsById("2")
+
+const productName = document.getElementById("name")
+const productPrice = document.getElementById("price")
+const form = document.getElementsByTagName("form")
+
+addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    await fetch("http://localhost:3333/products", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id: new Date().getTime().toString(),
+            name: productName.value,
+            price: productPrice.value,
+        }),
+    })
+    await fetchProducts()
+})
